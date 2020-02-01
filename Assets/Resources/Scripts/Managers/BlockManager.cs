110935964130b1ad11012e96b7a1c7d4f3c5dbd5
ashwinkdas007 <55556377+ashwinkdas007@users.noreply.github.com>
2020-02-01 01:30:52 -0500

    Added more stuff in for Building generation
    Added more stuff in for Building generation (In progress)

---
+++ Assets/Resources/Scripts/Managers/BlockManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : GenericManager<Block>
+{
+    #region
+    private static BlockManager instance;
+
+    private BlockManager() : base() { }
+
+    public static BlockManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new BlockManager();
+            }
+            return instance;
+        }
+    }
+    #endregion Singleton
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+    }
+
+    public override void UpdateManager(float dt)
+    {
+        base.UpdateManager(dt);
+
+    }
+}
